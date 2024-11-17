7d8fc516b51fb0c725d78d9e7799f8d600b2ca10
Tarek Auf der Strasse <69731049+codetakki@users.noreply.github.com>
2024-11-17 13:26:49 +0100

    Changed settings

--- Scripts/explosion.gd
+++ Scripts/explosion.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
-func _ready():
-	$AnimatedSprite2D.play("default")
-	$ExplosionSfx.play()  
 
 func _on_animated_sprite_2d_animation_finished():
 	queue_free()	
+	pass # Replace with function body.
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered():
+	$AnimatedSprite2D.play("default")
+	$ExplosionSfx.play()  
 	pass # Replace with function body.
