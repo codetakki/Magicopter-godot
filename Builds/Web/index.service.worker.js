4222143c9679a9ad3f80cf6b4170587a2c9922fc
Tarek Auf der Strasse <69731049+codetakki@users.noreply.github.com>
2024-12-03 16:15:06 +0100

    Web build

--- Builds/Web/index.service.worker.js
+++ Builds/Web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1733238807|392158182';
+const CACHE_VERSION = '1733238898|482913163';
 /** @type {string} */
 const CACHE_PREFIX = 'MagiCopter-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
